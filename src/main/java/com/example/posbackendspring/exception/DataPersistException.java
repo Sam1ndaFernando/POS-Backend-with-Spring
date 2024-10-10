49c9bc85048cde2c33a65fd15dbbb30865682384
Saami <uthpala2001@gmail.com>
2024-10-10 11:32:24 +0530

    DataPersistException

---
+++ src/main/java/com/example/posbackendspring/exception/DataPersistException.java
@@ -0,0 +1,12 @@
+package com.example.posbackendspring.exception;
+
+public class DataPersistException extends RuntimeException{
+    public DataPersistException() {
+    }
+
+    public DataPersistException(String message) {
+    }
+
+    public DataPersistException(String message, Throwable cause) {
+    }
+}
