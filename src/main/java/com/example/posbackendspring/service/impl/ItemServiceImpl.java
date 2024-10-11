02f2cc01d8f0c63986cf8bd3296e702cb15f37ae
Saami <uthpala2001@gmail.com>
2024-10-11 20:39:52 +0530

    saveItem done

---
+++ src/main/java/com/example/posbackendspring/service/impl/ItemServiceImpl.java
@@ -0,0 +1,54 @@
+package com.example.posbackendspring.service.impl;
+
+import com.example.posbackendspring.dto.ItemStatus;
+import com.example.posbackendspring.dto.impl.ItemDTO;
+import com.example.posbackendspring.entity.impl.ItemEntity;
+import com.example.posbackendspring.exception.DataPersistException;
+import com.example.posbackendspring.repository.ItemRepository;
+import com.example.posbackendspring.service.ItemService;
+import com.example.posbackendspring.util.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class ItemServiceImpl implements ItemService {
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Autowired
+    private Mapping mapping;
+    @Override
+    public void saveItem(ItemDTO itemDTO) {
+        ItemEntity item = itemRepository.save(mapping.toItemEntity(itemDTO));
+        if (item==null){
+            throw new DataPersistException("Item Note Saved");
+        }
+
+    }
+
+    @Override
+    public void updateItem(String itemId, ItemDTO itemDTO) {
+
+
+    }
+
+    @Override
+    public void deleteItem(String itemId) {
+
+    }
+
+    @Override
+    public ItemStatus getItem(String itemId) {
+        return null;
+    }
+
+    @Override
+    public List<ItemDTO> getAllItem() {
+        return null;
+    }
+}
