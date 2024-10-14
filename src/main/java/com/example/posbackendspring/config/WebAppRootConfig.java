abb12edaf970a3ffca0c236e06a9ee9ad6e04246
Saminda Fernando <uthpala2001@gmail.com>
2024-10-14 11:38:44 +0530

    transactionManager()

--- src/main/java/com/example/posbackendspring/config/WebAppRootConfig.java
+++ src/main/java/com/example/posbackendspring/config/WebAppRootConfig.java
@@ -1,12 +1,16 @@
 package com.example.posbackendspring.config;
 
+import jakarta.persistence.EntityManagerFactory;
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
@@ -36,6 +40,14 @@ public class WebAppRootConfig {
         entityManagerFactoryBean.setDataSource(dataSource());
         return entityManagerFactoryBean;
     }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory){
+        var transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
+    }
+
 
 
 }
