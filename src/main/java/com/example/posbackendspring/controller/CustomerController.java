9f453f7aea6b6c8df051306972a1d91c834389b7
Saminda Fernando <uthpala2001@gmail.com>
2024-10-14 11:53:33 +0530

    deleteCustomer()

--- src/main/java/com/example/posbackendspring/controller/CustomerController.java
+++ src/main/java/com/example/posbackendspring/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.example.posbackendspring.controller;
 import com.example.posbackendspring.dto.impl.CustomerDTO;
 import com.example.posbackendspring.exception.DataPersistException;
 import com.example.posbackendspring.service.CustomerService;
+import com.example.posbackendspring.util.Regex;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -42,5 +43,18 @@ public class CustomerController {
         }
     }
 
-
+    @DeleteMapping(value = "/{customerId}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable("customerId") String customerId){
+        try{
+            if (!Regex.customerIdValidate(customerId).matches()) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+            customerService.deleteCustomer(customerId);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }catch (DataPersistException e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
