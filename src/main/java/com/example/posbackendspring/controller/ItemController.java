2ff45126afe1bcae2aea9b10d4d42d256f57e657
Saminda Fernando <uthpala2001@gmail.com>
2024-10-14 12:09:18 +0530

    updateItem()

--- src/main/java/com/example/posbackendspring/controller/ItemController.java
+++ src/main/java/com/example/posbackendspring/controller/ItemController.java
@@ -30,6 +30,16 @@ public class ItemController {
         }
     }
 
-
+    @PutMapping(value = "/{itemCode}",consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> updateItem(@PathVariable("itemCode") String itemCode,@RequestBody ItemDTO itemDTO){
+        try{
+            itemService.updateItem(itemCode,itemDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }catch (DataPersistException e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
 }
