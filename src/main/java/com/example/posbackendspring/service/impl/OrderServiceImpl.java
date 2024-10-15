8c69dd9c96a9cf4d3be7d52cac5e2fba7666b4f4
Saami <uthpala2001@gmail.com>
2024-10-15 20:32:17 +0530

    getAllOrder()

--- src/main/java/com/example/posbackendspring/service/impl/OrderServiceImpl.java
+++ src/main/java/com/example/posbackendspring/service/impl/OrderServiceImpl.java
@@ -91,6 +91,7 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public List<OrderDTO> getAllOrder() {
-        return null;
+        return mapping.toOrderList(orderDao.findAll());
+
     }
 }
