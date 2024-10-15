9399098417edc7eee5e30a6966ef50ae539c956e
Saminda Fernando <uthpala2001@gmail.com>
2024-10-15 11:13:58 +0530

    saveOrder()

---
+++ src/main/java/com/example/posbackendspring/controller/OrderController.java
@@ -0,0 +1,35 @@
+package com.example.posbackendspring.controller;
+
+import com.example.posbackendspring.dto.impl.OrderDTO;
+import com.example.posbackendspring.exception.DataPersistException;
+import com.example.posbackendspring.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/orders")
+@CrossOrigin
+public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveOrder(@RequestBody OrderDTO orderDTO){
+        orderDTO.setOrderID(orderDTO.getOrderID());
+        try{
+            orderService.saveOrder(orderDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }catch (DataPersistException e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+
+}
