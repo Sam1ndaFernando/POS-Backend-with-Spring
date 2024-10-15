f227c4a67fa4616b1f642f58db75a6374a460837
Saminda Fernando <uthpala2001@gmail.com>
2024-10-15 11:50:40 +0530

    OrderDetailController created

---
+++ src/main/java/com/example/posbackendspring/controller/OrderDetailController.java
@@ -0,0 +1,17 @@
+package com.example.posbackendspring.controller;
+
+import com.example.posbackendspring.dto.ItemStatus;
+import com.example.posbackendspring.dto.impl.ItemDTO;
+import com.example.posbackendspring.dto.impl.OrderDetailDTO;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/orderDetails")
+@CrossOrigin
+public class OrderDetailController {
+
+
+
+}
