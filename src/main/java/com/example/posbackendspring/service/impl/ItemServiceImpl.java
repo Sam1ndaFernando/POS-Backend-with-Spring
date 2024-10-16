fcdce0c9531c2c4c5c209ec37f9af7186c2047c6
Saami <uthpala2001@gmail.com>
2024-10-16 11:42:04 +0530

    save Item with logging and error handling

--- src/main/java/com/example/posbackendspring/service/impl/ItemServiceImpl.java
+++ src/main/java/com/example/posbackendspring/service/impl/ItemServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.posbackendspring.service.impl;
 
+import com.example.posbackendspring.controller.ItemController;
 import com.example.posbackendspring.customStatusCode.ErrorStatus;
 import com.example.posbackendspring.dao.ItemDao;
 import com.example.posbackendspring.dto.ItemStatus;
@@ -10,6 +11,8 @@ import com.example.posbackendspring.exception.DataPersistException;
 import com.example.posbackendspring.dao.ItemDao;
 import com.example.posbackendspring.service.ItemService;
 import com.example.posbackendspring.util.Mapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,13 +28,17 @@ public class ItemServiceImpl implements ItemService {
 
     @Autowired
     private Mapping mapping;
+    private static final Logger logger = LoggerFactory.getLogger(ItemServiceImpl.class);
     @Override
     public void saveItem(ItemDTO itemDTO) {
+        logger.info("Save item with code: ", itemDTO.getItemCode());
         ItemEntity item = itemDao.save(mapping.toItemEntity(itemDTO));
         if (item==null){
-            throw new DataPersistException("Item Note Saved");
+            logger.error("Item could not be saved", itemDTO.getItemCode());
+            throw new DataPersistException("Customer Note Saved");
+        }else {
+            logger.info("Item has been saved successfully", itemDTO.getItemCode());
         }
-
     }
 
     @Override
