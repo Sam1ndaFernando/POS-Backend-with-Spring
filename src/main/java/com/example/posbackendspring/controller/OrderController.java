d851275cdf19885b689d97a49dd1e68d93da86b2
Saami <uthpala2001@gmail.com>
2024-10-16 11:04:30 +0530

    saveOrder() logger add

--- src/main/java/com/example/posbackendspring/controller/OrderController.java
+++ src/main/java/com/example/posbackendspring/controller/OrderController.java
@@ -6,6 +6,8 @@ import com.example.posbackendspring.dto.impl.OrderDTO;
 import com.example.posbackendspring.exception.DataPersistException;
 import com.example.posbackendspring.service.OrderService;
 import com.example.posbackendspring.util.Regex;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -21,17 +23,20 @@ public class OrderController {
 
     @Autowired
     private OrderService orderService;
-
+    static Logger logger = LoggerFactory.getLogger(OrderController.class);
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> saveOrder(@RequestBody OrderDTO orderDTO){
         orderDTO.setOrderID(orderDTO.getOrderID());
         try{
             orderService.saveOrder(orderDTO);
+            logger.error("Order Saved!");
             return new ResponseEntity<>(HttpStatus.CREATED);
         }catch (DataPersistException e){
+            logger.error("Bad Request!");
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }catch (Exception e){
             e.printStackTrace();
+            logger.error("Internal Server Error");
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
